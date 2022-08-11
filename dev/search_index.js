fe91d13a574b340ba2dd7d54b403709de630c038
Documenter.jl <documenter@juliadocs.github.io>
2022-08-11 08:38:14 +0000

    build based on 94ed40d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SecondOrderPerturbationTheory","category":"page"},{"location":"#SecondOrderPerturbationTheory","page":"Home","title":"SecondOrderPerturbationTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SecondOrderPerturbationTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SecondOrderPerturbationTheory]","category":"page"}]
+}
